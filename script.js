22529cf30fea0e6a0167758773c86e4ad3506a6f
Jonathas <jonathas.soliveira86@gmail.com>
2024-09-23 15:15:23 -0300

    tela de cadastro

---
+++ script.js
@@ -0,0 +1,27 @@
+function mostrarValor() {
+  const valorNome = document.querySelector('.name').value;
+  const valorEmail = document.querySelector('.email').value;
+  const valorTel = document.querySelector('.tel').value;
+  const valorSenha = document.querySelector('.senha').value;
+  const valorConfirmarSenha = document.querySelector('.confirmarSenha').value;
+  const valorCheckbox = document.querySelector('.checkbox').value;
+
+  console.log(
+    valorNome,
+    valorEmail,
+    valorTel,
+    valorSenha,
+    valorConfirmarSenha,
+    valorCheckbox
+  );
+}
+
+// Função chamada ao pressionar uma tecla no input
+function valorDoIpunt(event) {
+  const valorNome = document.querySelector('.name').value;
+  const valorEmail = document.querySelector('.email').value;
+  const valorTel = document.querySelector('.tel').value;
+  const valorSenha = document.querySelector('.senha').value;
+  const valorConfirmarSenha = document.querySelector('.confirmarSenha').value;
+  const valorCheckbox = document.querySelector('.checkbox').value;
+}
